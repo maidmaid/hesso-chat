bde5fe185f75da7ccc34c809c62f78992ff6e5fe
Dany <danymaillard93b@gmail.com>
2014-06-05 00:42:08 +0200

    #1 documentation

--- src/network/server/event/ClientEvent.java
+++ src/network/server/event/ClientEvent.java
@@ -2,8 +2,15 @@ package network.server.event;
 
 import java.util.EventObject;
 
+/**
+ * Event which indicates that an client connected occured.
+ * @author DM
+ */
 public class ClientEvent extends EventObject
 {
+	/**
+	 * Constructs an ClientEvent object.
+	 */
 	public ClientEvent(Object source)
 	{
 		super(source);
